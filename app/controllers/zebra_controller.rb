45ea64aab3c95dd83c59116f4b536a6383966963
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-09-24 17:46:21 +0000

    set-up template, follow and run

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,6 +1,19 @@
 class ZebraController < ApplicationController
   def giraffe
     @random_move = ["rock", "paper", "sissors"].sample
+
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def elephant
+    @random_move = ["rock", "paper", "sissors"].sample
+
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def lion
+    @random_move = ["rock", "paper", "sissors"].sample
+    
     render({ :template => "game_templates/play_rock" })
   end
 end
