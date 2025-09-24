a5a21ce577dbee59a72a649d083d47121fa96673
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-09-24 17:25:08 +0000

    created an instance veriable in the action @rand_move in zebra_controller.rb file

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,6 @@
 class ZebraController < ApplicationController
   def giraffe
+    @random_move = ["rock", "paper", "sissors"].sample #create instance variable in action
     render({ :template => "game_templates/play_rock" })
   end
 end
