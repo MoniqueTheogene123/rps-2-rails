5707098356826304ac81cd6e34323008c152c23b
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-09-24 17:27:17 +0000

    yes

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,6 +1,6 @@
 class ZebraController < ApplicationController
   def giraffe
-    @random_move = ["rock", "paper", "sissors"].sample #create instance variable in action
+    @random_move = ["rock", "paper", "sissors"].sample
     render({ :template => "game_templates/play_rock" })
   end
 end
