27ff68cbd4e4c91dadfccf317a1353f631fb9785
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-09-25 14:29:16 +0000

    added templates to make dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,60 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
 end
+
+
+# require "sinatra"
+# require "sinatra/reloader"
+
+# get("/") do
+
+#   erb(:homepage)
+# end
+
+# get("/rock") do
+#   moves = ["rock", "paper", "scissors"]
+
+#   @comp_move = moves.sample
+  
+#   if @comp_move == "rock"
+#     @outcome = "tied"
+#   elsif @comp_move == "paper"
+#     @outcome = "lost"
+#   else
+#     @outcome = "won"
+#   end
+
+#   erb(:zebra)
+# end
+
+# get("/paper") do
+#     moves = ["rock", "paper", "scissors"]
+
+#   @comp_move = moves.sample
+  
+#   if @comp_move == "paper"
+#     @outcome = "tied"
+#   elsif @comp_move == "rock"
+#     @outcome = "won"
+#   else
+#     @outcome = "lost"
+#   end
+
+#   erb(:giraffe)
+# end
+
+# get("/scissors") do
+#     moves = ["rock", "paper", "scissors"]
+
+#   @comp_move = moves.sample
+  
+#   if @comp_move == "scissors"
+#     @outcome = "tied"
+#   elsif @comp_move == "paper"
+#     @outcome = "won"
+#   else
+#     @outcome = "lost"
+#   end
+
+#   erb(:bear)
+# end
